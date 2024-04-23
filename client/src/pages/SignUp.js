ce1e312c3c8a9b0e9a45a8c36116d0273dfe93f3
ChangwooJ <wjdckddn99@naver.com>
2024-04-23 12:14:41 +0900

    단순 회원가입 페이지 제작 완료

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -24,7 +24,7 @@ const SignUp = () => {
                                     <input type="text" placeholder="아이디를 입력해주세요" />
                                 </div>
                                 <div className="check_button">
-                                    <input className="bt_style" type="button" value="중복확인"/>
+                                    <input className="bt_style" type="button" value="중복확인" />
                                 </div>
                             </div>
                             <div className="signup_pw">
@@ -51,7 +51,7 @@ const SignUp = () => {
                                     <input type="text" placeholder="예: marketkurly@kurly.com" />
                                 </div>
                                 <div className="check_button">
-                                    <input className="bt_style" type="button" value="중복확인"/>
+                                    <input className="bt_style" type="button" value="중복확인" />
                                 </div>
                             </div>
                             <div className="signup_ph">
@@ -60,13 +60,13 @@ const SignUp = () => {
                                     <input type="text" placeholder="숫자만 입력해주세요" />
                                 </div>
                                 <div className="check_button">
-                                    <input className="bt_style" type="button" value="인증번호 받기"/>
+                                    <input className="bt_style" type="button" value="인증번호 받기" />
                                 </div>
                             </div>
                             <div className="signup_address">
                                 <div className="title">주소<span className="star">*</span></div>
                                 <div className="address info">
-                                    <input className="bt_style" type="button" value="주소 검색"/>
+                                    <input className="bt_style" type="button" value="주소 검색" />
                                 </div>
                             </div>
                             <div className="address_plus">
@@ -76,15 +76,15 @@ const SignUp = () => {
                             <div className="signup_gender">
                                 <div className="title">성별</div>
                                 <div className="gender">
-                                    <div className="man"><input type="radio" name="gender" value="남자"/><span>남자</span></div>
-                                    <div className="woman"><input type="radio" name="gender" value="여자"/><span>여자</span></div>
-                                    <div className="none"><input type="radio" name="gender" value="선택안함" checked/><span>선택안함</span></div>
+                                    <div className="man"><input type="radio" name="gender" value="남자" /><span>남자</span></div>
+                                    <div className="woman"><input type="radio" name="gender" value="여자" /><span>여자</span></div>
+                                    <div className="none"><input type="radio" name="gender" value="선택안함" checked /><span>선택안함</span></div>
                                 </div>
                             </div>
                             <div className="signup_birth">
                                 <div className="title">생년월일</div>
                                 <div className="birth_date info"> {/* input date랑 다르다고 판단. number의 글자수 제한 안됨. */}
-                                    <input type="number" placeholder="YYYY"/><div>/</div>
+                                    <input type="number" placeholder="YYYY" /><div>/</div>
                                     <input type="number" placeholder="MM" /><div>/</div>
                                     <input type="number" placeholder="DD" />
                                 </div>
@@ -96,8 +96,54 @@ const SignUp = () => {
                                 <span className="star">*</span>
                             </div>
                             <div className="terms_body">
-                                <div></div>
+                                <div className="fir">
+                                    <input type="checkbox" id="check1"/>
+                                      <label for="check1"></label>
+                                    <span>전체 동의합니다.</span>
+                                    <p>선택항목에 동의하지 않은 경우도 회원가입 및 일반적인 서비스를 이용할 수 있습니다.</p>
+                                </div>
+                                <div className="sec">
+                                    <input type="checkbox" id="check2"/>
+                                      <label for="check2"></label>
+                                    <span> 이용약관 동의</span>
+                                    <span className="sub">(필수)</span>
+                                </div>
+                                <div className="sec">
+                                    <input type="checkbox" id="check3"/>
+                                      <label for="check3"></label>
+                                    <span> 개인정보 수집·이용 동의</span>
+                                    <span className="sub">(필수)</span>
+                                </div>
+                                <div className="sec">
+                                    <input type="checkbox" id="check4"/>
+                                      <label for="check4"></label>
+                                    <span> 개인정보 수집·이용 동의</span>
+                                    <span className="sub">(선택)</span>
+                                </div>
+                                <div className="thr">
+                                    <input type="checkbox" id="check5"/>
+                                      <label for="check5"></label>
+                                    <span> 무료배송, 할인쿠폰 등 혜택/정보 수신 동의</span>
+                                    <span className="sub">(선택)</span>
+                                    <div className="thr_sub">
+                                        <input type="checkbox" id="check6"/>
+                                          <label for="check6"></label>
+                                        <span>SMS</span>
+                                        <input type="checkbox" id="check7"/>
+                                          <label for="check7"></label>
+                                        <span>이메일</span>
+                                    </div>
+                                </div>
+                                <div className="sec">
+                                    <input type="checkbox" id="check8"/>
+                                      <label for="check8"></label>
+                                    <span> 본인은 만 14세 이상입니다.</span>
+                                    <span className="sub">(필수)</span>
+                                </div>
                             </div>
+                        </div>
+                        <div className="signup_bt_wrap">
+                            <input type="submit" value="가입하기" />
                         </div>
                     </form>
                 </div>
