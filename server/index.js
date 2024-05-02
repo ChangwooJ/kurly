0654964b47764ad982bf292a4607cc2a5b38af12
ChangwooJ <wjdckddn99@naver.com>
2024-05-02 20:14:02 +0900

    세션 인증 방식 구현중

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 const PORT = process.env.PORT || 8000;
 const router = require('./router/routes');
 
 app.use(cors());
 app.use(express.json());
+
+app.use(session({
+    secret: 'kurly-jung',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore(),
+    cookie: {
+        domain: 'localhost',
+        path: '/',
+        maxAge: 24 * 6 * 60 * 10000,
+        sameSite: 'none',
+        httpOnly: true,
+        secure: true,
+      },
+}));
 
 app.use('/api', router);
 
--- server/router/routes.js
+++ server/router/routes.js
@@ -6,11 +6,13 @@ const getReviewList = require('../handlers/reviewHandler');
 const getQnaList = require('../handlers/qnaHandler');
 const getUserList = require('../handlers/userHandler');
 const postSignupList = require('../handlers/signupHandler');
+const getLoginList = require('../handlers/loginHandler');
 
 router.get('/item', getItemList);
 router.get('/reviews', getReviewList);
 router.get('/qnas', getQnaList);
 router.get('/users', getUserList);
 router.post('/signup', postSignupList);
+router.post('/login', getLoginList);
 
 module.exports = router;
